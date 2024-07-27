a54a8b7be6a3a45f3bebcb881aae2a8b76d660bd
Jessica Duarte <arq.jessica.duarte@gmail.com>
2024-07-27 12:20:55 +0200

    Main container

--- index.js
+++ index.js
@@ -15,7 +15,28 @@ const getCharacters = async () => {
   return data
 }
 
-
+const characterTemplate = /*html*/`
+<div class="character-container">
+  <div class="character-image">
+    <img> 
+  </div>
+  <div class="character-description">
+    <div>
+      <h2>Nombre</h2>
+      <div>Status</div>
+    </div>
+    <div>
+      <div class="character-tag">Last known Location:</div>
+      <p>Location</p>
+    </div>
+    <div>
+      <div class="character-tag">First seen in:</div>
+      <p>Episodio</p>
+    </div>
+  </div>
+</div>
+`
+$mainSection.innerHTML = characterTemplate
 
 
 getCharacters()
