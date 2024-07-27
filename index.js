061b2e69182b5224750e7ffa6dea16054d29a4fa
Dani <daniela.gomezp05@outlook.com>
2024-07-27 12:52:21 +0200

    Feat: adding rendering template based on data

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-// HTML elements
-const $mainSection = document.querySelector(".main-section");
+(async () => {
+  // HTML elements
+  const $mainSection = document.querySelector(".main-section");
+
+  // Get Data
+  const getCharacters = async () => {
+    const API_URL = "https://rickandmortyapi.com/api/character";
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  };
 
 
-// Get Data
-const getCharacters = async () => {
-  const API_URL = "https://rickandmortyapi.com/api/character"
-  const response = await fetch(API_URL)
+  const createCharacters = (data, htmlToInsert) => {
+    console.log(data)
+    const characterHTMLElements = data.map((character) => {
+      return /*html*/ `
+      <div class="character-container">
+        <div class="character-image">
+          <img src="${character.image}">
+        </div>
+        <div class="character-description">
+          <div>
+            <h2>${character.name}</h2>
+            <div>${character.status}</div>
+          </div>
+          <div>
+            <div class="character-tag">Last known Location:</div>
+            <p>${character.origin.name}</p>
+          </div>
+          <div>
+            <div class="character-tag">First seen in:</div>
+            <p>Episodio</p>
+          </div>
+        </div>
+      </div>
+    `;
+    });
+
+    htmlToInsert.innerHTML = characterHTMLElements.join("");
+  };
 
 
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`)
-  }
-  const data = await response.json()
-  return data
-}
+  const characters = await getCharacters();
 
-const characterTemplate = /*html*/`
-<div class="character-container">
-  <div class="character-image">
-    <img> 
-  </div>
-  <div class="character-description">
-    <div>
-      <h2>Nombre</h2>
-      <div>Status</div>
-    </div>
-    <div>
-      <div class="character-tag">Last known Location:</div>
-      <p>Location</p>
-    </div>
-    <div>
-      <div class="character-tag">First seen in:</div>
-      <p>Episodio</p>
-    </div>
-  </div>
-</div>
-`
-$mainSection.innerHTML = characterTemplate
+  createCharacters(characters.results, $mainSection)
 
-
-getCharacters()
+})();
